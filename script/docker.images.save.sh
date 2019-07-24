79ea2ae20b1289d1b27d3e52defedd1a95acb9a4
yangyang <www.yangyang.com@qq.com>
2019-07-23 22:37:11 -0400

    package allimage's script

---
+++ script/docker.images.save.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#获取docker images
+
+tags=(`docker images | awk 'NR!=1{print $2}' `)
+images=(`docker images | awk 'NR!=1{print $1}' `)
+
+lenth=${#images[@]}
+for ((i=0;i<$lenth;i++ ));
+do 
+#echo ${images[$i]}:${tags[$i]}
+ifdomain=`echo  ${images[$i]} | grep '/' |wc -l `
+
+#echo $ifdomain 
+if [ $ifdomain -gt 0 ]
+then 
+tarname=`echo ${images[$i]} |awk -F '/' '{ print $NF }'` 
+else
+tarname=${images[$i]}
+
+fi 
+
+echo $tarname 
+docker save -o $tarname'.tar'  ${images[$i]}:${tags[$i]}
+
+done 
+
+
+mv  *.tar  /k8s/git-k8s/images/
